80abdc4755d6d2eee711df7ac418475f8dc419ad
ana.borela <anaborela1@gmail.com>
2021-10-08 10:35:43 -0300

    alterações views

--- resources/views/front/seti.blade.php
+++ resources/views/front/seti.blade.php
@@ -34,16 +34,17 @@
             <div class="row featurette">
 
                 <!-- Consultoria -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/consultoria-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="1">
+                <div class="col-md-12 px-5" id="1">
                     <h2 class="py-3">Consultoria em TI</h2>
                     <p>
                         Milhares de empresas são fechadas anualmente por falta de PLANEJAMENTO que é imprescindível quando
                         se fala em gestão e crescimento. Seja a longo, médio ou curto prazo, a Unika desenvolve Plano
                         Diretor de TI e Comunicação - PDTI&C, atuando em toda estrutura ligada a TI, desde planejamento de
                         compras até projetos, oferecendo:
+                    </p>
+                </div>
+
+                <div class="col-md-6 col-12">
                     <ul>
                         <li>Análise para investimento em equipamentos: rede de dados, Servidores, Telefonia, Impressão e
                             equipamentos.</li>
@@ -53,16 +54,12 @@
                         <li>Processos para melhor utilização de recursos (impressão, energia etc)</li>
                         <li>Emails corporativos</li>
                     </ul>
-                    </p>
                 </div>
 
                 <hr class="featurette-divider my-5">
 
                 <!-- Redes -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/redes-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="2">
+                <div class="col-md-12 px-5" id="2">
                     <h2 class="py-3">Redes e Conectividade</h2>
                     <p>
                         Em sua empresa ou residência, a rede de computadores deve ser devidamente protegida para que o(s)
@@ -71,22 +68,22 @@
                         acesso tanto aos arquivos quanto a internet serão facilitados. Com proteção adequada a(s) sua(s)
                         máquina(s), é possível evitar vírus, os maiores causadores de danos. A Unika monitora, configura e
                         cuida do suporte de sua rede com eficiência e segurança. Temos ferramentas para:
+                    </p>
+                </div>
+                
+                <div class="col-md-12">
                     <ul>
                         <li>Inventário de equipamentos na rede</li>
                         <li>Alerta de vírus remotamente</li>
                         <li>Configuração de antivírus corporativo</li>
                         <li>e muitas outras...</li>
                     </ul>
-                    </p>
                 </div>
 
                 <hr class="featurette-divider my-5">
 
                 <!-- Assistencia -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/assistencia-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="3">
+                <div class="col-md-12 px-5" id="3">
                     <h2 class="py-3">Assistência técnica</h2>
                     <p>
                         Para maior durabilidade da sua máquina, seja computador, notebook ou qualquer outro equipamento de
@@ -95,6 +92,10 @@
                         funcionamento convencional. Para evitar danos irreparáveis, faz-se fundamental essa assistência
                         técnica que teve ser feita por profissionais capacitados e treinados para tal. Nossos clientes tem
                         acesso à:
+                    </p>
+                </div>
+
+                <div class="col-md-12">
                     <ul>
                         <li>Manutenção preventiva (detectar e resolver problemas antes que ocorram)</li>
                         <li>Manutenção corretiva</li>
@@ -102,37 +103,34 @@
                         <li>Atendimento via e-Mail/Telefone</li>
                         <li>Atendimento remoto</li>
                     </ul>
-                    </p>
                 </div>
 
                 <hr class="featurette-divider my-5">
 
                 <!-- Backup -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/backup-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="4">
+                <div class="col-md-12 px-5" id="4">
                     <h2 class="p-3">Soluções de Backup</h2>
                     <p>
                         Dados e informações são valiosíssimos tanto no trabalho quanto na vida pessoal. Empresários precisam
                         ter controle dos seus dados e arquivos. Sem esquecer as fotos pessoais, recordações familiares.
                         Existem diversos meios de assegurar que esses preciosos arquivos estejam seguros. Dentre eles, o
                         backup sendo o mais funcional. A Unika viabiliza a segurança de seus arquivos.
+                    </p>
+                </div>
+
+                <div class="col-md-6 col-12">
                     <ul>
                         <li>Espelhamento Local</li>
                         <li>Upload para nuvem</li>
                     </ul>
-                    </p>
-                    <p>Prevenir é sempre a melhor escolha.</p>
+                    <p style="padding-left: 37px">Prevenir é sempre a melhor escolha.</p>
                 </div>
+
 
                 <hr class="featurette-divider my-5">
 
                 <!-- Helpdesk -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/helpdesk-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="5">
+                <div class="col-md-12 px-5" id="5">
                     <h2 class="py-3">Helpdesk local</h2>
                     <p>
                         Para maior durabilidade da sua máquina, seja computador, notebook ou qualquer outro equipamento de
@@ -141,6 +139,10 @@
                         funcionamento convencional. Para evitar danos irreparáveis, faz-se fundamental essa assistência
                         técnica que teve ser feita por profissionais capacitados e treinados para tal. Nossos clientes tem
                         acesso à:
+                    </p>
+                </div>
+
+                <div class="col-md-6 col-12">
                     <ul>
                         <li>Manutenção preventiva (detectar e resolver problemas antes que ocorram)</li>
                         <li>Manutenção corretiva</li>
@@ -148,35 +150,39 @@
                         <li>Atendimento via e-Mail/Telefone</li>
                         <li>Atendimento remoto</li>
                     </ul>
-                    </p>
                 </div>
 
                 <hr class="featurette-divider my-5">
 
                 <!-- Servidores -->
-                <div class="col-md-5 col-sm-12 p-4">
-                    <img class="seti-images hvr-grow" src="{{ asset('images/seti/servidor-min.jpg') }}" alt="teste">
-                </div>
-                <div class="col-md-12 col-lg-6 px-5" id="6">
+                <div class="col-md-12 px-5" id="6">
                     <h2 class="py-3">Manutenção de servidores</h2>
                     <p>
                         É de fundamental importância para o bom funcionamento de uma empresa o monitoramento com
                         detalhamento. Desde um simples acesso a sites indesejados, a monitoramento de câmeras de segurança.
                         A Unika configura redes de acordo com a necessidade de cada empresa e cliente com plena segurança.
                         Configuramos servidores na plataforma do Linux e Windows, dentre os quais:
+                    </p>
+                </div>
+
+                <div class="col-md-6 col-12">
                     <ul>
                         <li>Serviço de Diretório (contas de usuários unificadas)</li>
                         <li>FTP (Transferência de arquivos via rede com segurança e velocidade)</li>
                         <li>Aplicação (Centralização de aplicativos da empresa)</li>
                         <li>Terminal (Acesso a serviços de terminal remoto)</li>
                         <li>Proxy (acesso a sites e informações na rede)</li>
+                    </ul>
+                </div>
+
+                <div class="col-md-6 col-12">
+                    <ul>
                         <li>Firewall (Controle de entradas e saídas de rede)</li>
                         <li>VPN (Integração site to site e site to client)</li>
                         <li>Arquivos (centralizar informações da empresa)</li>
                         <li>Virtualização (servidores e desktops)</li>
                         <li>Web Server (Hospedagem LAMP)</li>
                     </ul>
-                    </p>
                 </div>
 
             </div>
@@ -191,99 +197,99 @@
                     <h1>Clientes</h1>
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/bandeirantes-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/cell-center-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/iso-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/lm-supermercados-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/nhdfoods-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/fundacao-triangulo-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/talaricos-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/aciu-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/cartorio-protesto-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/cartorio-registro-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/cdl-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/rg-contabilidade-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/ag-auditoria-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/cozinha-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/doidao-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/efop-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/fit-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/goodsoy-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/jcoadvocacia-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/ultragaz-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/mac-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/oab-min.jpg') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/omni-min.png') }}" alt="">
                 </div>
 
-                <div class="col-12 col-md-2 p-0">
+                <div class="col-12 col-md-2 py-2">
                     <img class="partners-images" src="{{ asset('images/parceiros/penergetic-min.png') }}" alt="">
                 </div>
 
